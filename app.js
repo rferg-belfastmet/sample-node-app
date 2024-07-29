18c60a887b9c5d90b792d083f1de1397de97801d
rferg-belfastmet <50098143@belfastmet.ac.uk>
2024-07-29 16:06:15 +0100

    New update to the message

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var server = require('http').Server(app);
 port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send("Hello World!!! Node Sample App is up and running!");
+    res.send("We are learning!");
 });
 
 app.get('/ping', (req, res) => {
